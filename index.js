8b754e42a265ac58ac0d4969c219118ac196db9e
U-EMERGEDESK\Franck <franckbinard@gmail.com>
2019-04-07 22:33:36 -0400

    general app framework

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
 const path = require('path');
+
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static('public'));
+var hbs = exphbs.create({
+    defaultLayout: 'main', 
+    partialsDir: [
+        'views/partials'
+    ]
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 app.get('/', function (req, res){
-	res.sendFile(path.join(__dirname, '/public/edit.html'));
+    res.render('home');
 });	
-	
+app.use(express.static('public/'));	
 app.listen(port, ()=> console.log(`Example app listening on port ${port}`));
 
 
---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+AJS.toInit(function($){
+
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.session.setMode("ace/mode/javascript");
+
+
+});
