3aa4815281788484cb69412a915043a58aa4a200
U-EMERGEDESK\Franck <franckbinard@gmail.com>
2019-04-12 23:08:47 -0400

    added express router

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
+
+const router = express.Router();
+router.param(function(param, option){
+    return function(req, res, next, val){
+        if (val == option){next();}
+        else {res.sendStatus(403);}
+  }
+});
+
+router.param('id', 1337);       
 
 const path = require('path');
-
-
 const port = process.env.PORT || 3000;
 const app = express();
 
-var hbs = exphbs.create({
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({
     defaultLayout: 'main', 
     partialsDir: [
         'views/partials'
     ]
 });
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 
-app.get('/', function (req, res){
+router.get('/', function (req, res){
     res.render('home');
 });	
-app.use(express.static('public/'));	
-app.listen(port, ()=> console.log(`Example app listening on port ${port}`));
+
+app.use(router);
+
+var publicFolderOptions = {
+    dotfiles: 'ignore', 
+    etag: false, 
+    extensions:['htm', 'html'], 
+    index: false, 
+    maxAge: '1d', 
+    redirect: false, 
+    setHeaders: function(res, path, stat){
+        res.set('x-timestamp', Date.now())
+    }
+}
+app.use(express.static('public/', publicFolderOptions));	
+app.listen(port, ()=> console.log(`lubriderm listening on port ${port}`));
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,37 @@
+
+var um = (function(){
+
+
+    return {
+        umProperties: function(){
+        },
+        umText: function(){
+        }
+    }
+})();
+
+
+var uploadUserMacroText = function(){
+   //if um.umProperties.newFile...
+   //   um.createNewFile()
+   //   um.upload();
+   //
+   //else
+   //   if(um.warnOverwrite()){um.upload()};
+   alert("hello");
+}
 AJS.toInit(function($){
+  AJS.$("#edUpload").click(function(e) {
+        e.preventDefault();
+        uploadUserMacroText();
+    });
+
 
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
     editor.session.setMode("ace/mode/javascript");
 
 
+
+    
 });
